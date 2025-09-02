f329dc657dc61a06b3695dd775ea10247e6c03fc
ClementGuer <clement.guerrero@viacesi.fr>
2025-09-02 10:18:14 +0200

    correction des tests en ajoutant un role.

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\Role;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,11 +12,13 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register(): void
     {
+        $role = Role::factory()->create();
         $response = $this->postJson('/api/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'role_id' => $role->id,
         ]);
 
         $response->assertStatus(201);
